b7ae7461c56a9fcb2807e0094113acb0692b0a6e
arikakira <samanthal242@nycstudents.net>
2024-09-13 15:33:45 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Github doesn't work for me. There's no run button for me.");
+        System.out.print(":( ");
+        System.out.print("Replit works better, can I use it instead?");
+    }
+}
